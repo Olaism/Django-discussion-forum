298285644d12724e0cdaf1593be06f7511ba230f
= <=>
2023-09-16 01:52:48 +0100

    implement new_topic form template and logic

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boards.apps.BoardsConfig'
+    'widget_tweaks',
+    'boards.apps.BoardsConfig',
 ]
 
 MIDDLEWARE = [
