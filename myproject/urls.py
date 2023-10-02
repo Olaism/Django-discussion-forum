0f710be57ae30c87f25b0b3c691405ef3467ba37
= <=>
2023-10-02 23:09:57 +0100

    add tests to password_reset_view

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.contrib.auth import views as auth_views
 
 from accounts import views as account_views
@@ -12,5 +12,9 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
     path('boards/<int:pk>/', board_views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', board_views.new_topic, name='new_topic'),
+    path('password/reset/', include([
+        path('', auth_views.PasswordResetView.as_view(template_name='password_reset.html', email_template_name='password_reset_email.html', subject_template_name='password_reset_subject.txt'), name='password_reset'),
+        path('done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    ])),
     path('admin/', admin.site.urls),
 ]
