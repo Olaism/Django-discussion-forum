93daa2a7771da53e1707e16b7865c4979fba1ecb
= <=>
2023-10-18 10:28:29 +0100

    add complete tests for topic_posts

--- boards/tests/test_view_topic_posts.py
+++ boards/tests/test_view_topic_posts.py
@@ -7,16 +7,22 @@ from ..views import PostListView
 
 User = get_user_model()
 
-
-class TopicPostsTests(TestCase):
+class TopicPostsViewTestCase(TestCase):
     
     def setUp(self):
         self.board = Board.objects.create(name="test", description="test for topic posts")
         user = User.objects.create_user(username="testuser", email="testuser@mail.com", password="testpassword")
         self.topic = Topic.objects.create(subject="test subject", board=self.board, starter=user)
-        Post.objects.create(message="lorem ipsum dolor sit amet", topic=self.topic, created_by=user)
-        url = reverse('topic_posts', kwargs={"pk": self.board.pk, 'topic_pk': self.topic.board.pk})
-        self.response = self.client.get(url)
+        self.post = Post.objects.create(message="lorem ipsum dolor sit amet", topic=self.topic, created_by=user)
+        self.url = reverse('topic_posts', kwargs={"pk": self.board.pk, 'topic_pk': self.topic.pk})
+        self.edit_link = reverse('edit_post', kwargs={'pk': self.board.pk, 'topic_pk': self.topic.pk, 'post_pk': self.post.pk})
+
+
+class TopicPostsTests(TopicPostsViewTestCase):
+    
+    def setUp(self):
+        super().setUp()
+        self.response = self.client.get(self.url)
 
     def test_status_code(self):
         self.assertEquals(self.response.status_code, 200)
@@ -30,10 +36,13 @@ class TopicPostsTests(TestCase):
         self.assertContains(self.response, reply_url)
 
     def test_view_contains_edit_link(self):
-        pass
+        User.objects.create_user(username='otheruser', email='otheruser@gmail.com', password='otherpassword')
+        self.client.login(username='otheruser', password='otherpassword')
+        self.response = self.client.get(self.url)
+        self.assertNotContains(self.response, self.edit_link)
 
-    def test_view_does_not_contain_edit_link_for_unauthorized_user(self):
-        pass
+    def test_view_does_not_contain_edit_link_for_not_authenticated_user(self):
+        self.assertNotContains(self.response, self.edit_link)
     
     
     
